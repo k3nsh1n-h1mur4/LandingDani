a2f7cdc071e2798a674b5955aba05aa1d8b7df39
k3nsh1n-h1mur4 <isaac.acervantes@gmail.com>
2025-12-17 21:24:48 -0600

    Add Buzon Electronico finally

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,19 +1,32 @@
-import { defineAction } from "astro:actions";
+import { defineAction, ActionError } from "astro:actions";
 import { z } from "astro:schema";
+import { supabase } from "@helpers/SUpabaseClient.js";
 
 
 export const server = {
     sendMail: defineAction({
         accept: 'form',
         input: z.object({
-            userName: z.string(),
+            name: z.string(),
             email: z.string().email(),
             message: z.string()
         }),
-        handler: async (res) => {
-            const { userName, email, message } = res;
-            console.log(userName, email, message)
-
-        }
+        handler: async ({ name, email, message }) => {
+            const {  error } = await supabase.from('buzon').insert({
+                name: name,
+                email: email,
+                message: message,
+            })
+            if (error) {
+                throw new ActionError({
+                    code: 'BAD_REQUEST',
+                    message: 'Error al enviar el mensaje',
+                });
+            }
+            return ({
+                success: true,
+                message: 'Mensaje enviado correctamente',
+            })    
+        },
     })
 }
--- src/helpers/SUpabaseClient.js
+++ src/helpers/SUpabaseClient.js
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = 'https://favhepjcoopuoyzqefsi.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZhdmhlcGpjb29wdW95enFlZnNpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ1NzIxMzQsImV4cCI6MjA4MDE0ODEzNH0.oL1F2tUyTrRAIKAKxzvXLDncZ-YkrAZfHmaIV0jhVHc'
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
-console.log(supabase)
-export default supabase;
+export const supabase = createClient(
+    import.meta.env.SUPABASE_URL,
+    import.meta.env.SUPABASE_KEY,
+);
