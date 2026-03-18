d040a9b5bc6523c7494274616e10280b11b9f50d
k3nsh1n-h1mur4 <isaac.acervantes@gmail.com>
2026-03-18 13:30:01 -0600

    turso url

--- src/turso.ts
+++ src/turso.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@libsql/client/web";
 
 export const turso = createClient({
-  url: import.meta.env.TURSO_DATABASE_URL,
+  url: import.meta.env.TURSO_DATABASE_URL!,
   authToken: import.meta.env.TURSO_AUTH_TOKEN,
 });
