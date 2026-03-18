46883960748b7a137d3154314e9d73caf015c7c5
k3nsh1n-h1mur4 <isaac.acervantes@gmail.com>
2026-03-18 12:48:33 -0600

    Adding turso db

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -2,7 +2,7 @@ import { defineAction, ActionError } from "astro:actions";
 import { z } from "astro:schema";
 import { supabase } from "@helpers/SUpabaseClient.js";
 import { cryptEmail } from "@utils/CryptEmail.js";
-
+import { turso } from '../turso.ts';
 
 export const server = {
     sendMail: defineAction({
@@ -12,17 +12,27 @@ export const server = {
             email: z.string().email(),
             message: z.string()
         }),
-        handler: async ( input ) => {
+        handler: async (input) => {
             const name = input.name;
             const email = await cryptEmail(input.email);
             const message = input.message;
             console.log(email)
-            const {  error } = await supabase.from('buzon').insert({
-                name: name,
+            //const {  error } = await supabase.from('buzon').insert({
+            const result = await turso.batch(
+                [
+                    {
+                        sql: 'INSERTO INTO buzon(name, email, message) VALUES(?, ?, ?)',
+                        args: [name, email, message],
+                    },
+                ],
+                "write",
+            );
+            console.log(result);
+            /*name: name,
                 email: email,
-                message: message,
-            })
-            if (error) {
+                    message: message,
+            })*/
+            if (!result) {
                 throw new ActionError({
                     code: 'BAD_REQUEST',
                     message: 'Error al enviar el mensaje',
@@ -31,7 +41,7 @@ export const server = {
             return ({
                 success: true,
                 message: 'Mensaje enviado correctamente',
-            })    
+            })
         },
     })
 }
