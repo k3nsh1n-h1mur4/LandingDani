ad5ccd1591fd04a53ba332bd23d42811c6abc041
k3nsh1n-h1mur4 <isaac.acervantes@gmail.com>
2026-03-18 13:57:42 -0600

    import createCLient from libsql

--- src/lib/tursoDb.ts
+++ src/lib/tursoDb.ts
@@ -1,7 +1,10 @@
-import { createClient } from "@libsql/client/web";
+//import { createClient } from "@libsql/client/web";
+//import { createClient } from "@libsql/client/http";
+
+import { createClient } from "@libsql/client";
 
 
 export const client = createClient({
-  url: import.meta.env.TURSO_DATABASE_URL,
-  authToken: import.meta.env.TURSO_AUTH_TOKEN,
+  url: import.meta.env.ASTRO_REMOTE_DATABASE_URL,
+  authToken: import.meta.env.ASTRO_REMOTE_AUTH_TOKEN,
 })
