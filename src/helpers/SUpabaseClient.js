823947ca7710d2efdb028da58ab4950cc4c82425
k3nsh1n-h1mur4 <isaac.acervantes@gmail.com>
2026-03-18 13:03:50 -0600

    delete supabaseclient

--- src/helpers/SUpabaseClient.js
+++ src/helpers/SUpabaseClient.js
@@ -1,7 +1,7 @@
-import { createClient } from "@supabase/supabase-js";
+/*import { createClient } from "@supabase/supabase-js";
 
 
 export const supabase = createClient(
     import.meta.env.SUPABASE_URL,
     import.meta.env.SUPABASE_KEY,
-);
+);*/
